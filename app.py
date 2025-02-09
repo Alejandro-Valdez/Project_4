0414447843af1b1391a33b196e435f98af8cc8c9
Alejandro-Valdez <alejandrodv0304@gmail.com>
2025-02-09 13:33:54 -0500

    second

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ import numpy as np
 
 # these are stateful variables which are preserved as Streamlit reruns this script
 st.header("Comparing Data Among Multiple Vehicles")
+
 vehicles_dt = pd.read_csv('./data/vehicles_us.csv') #assigns the vehicle dataset from Noteboooks to value 'vehicles_dt'
-st.write(vehicles_dt.head())
+
+st.write(vehicles_dt.head()) #displays the first five lines of the 'vehicles_dt' dataframe
 # Making a histogram showing the information of how the transmission type of the vehicle 'corelates' to the days listed possibly showing what type of transmission is more or least desireable 
+
+st.title("Days Since Car Was Listed VS the Transmission Type of the Car")
 
 # Creating the information sets for the histogram to pull data from
 p1 =vehicles_dt[vehicles_dt['transmission'] == 'automatic' ][ 'days_listed'] # Gathers data from the dataframe and pulls specifically the days listed from the cars the report to have 'automatic' transmission and assigns it
@@ -33,5 +37,16 @@ fig.update_xaxes(title_text="Days Since Listed") # Updates the X Axis to be titl
 fig.update_yaxes(title_text="Amount") # Updates the Y axis to be title "Amount"
 st.write(fig) # Displays the Chart
 
+st.title('Cars Price in Comparison to its Odometer Reading by Brand')
 check = st.checkbox('Toggle line for graph')
 st.write('State of the check box:', check)
+
+fig_sc = px.scatter(vehicles_dt, x="price", y="odometer", color="brand",
+                  hover_data=['cylinders'])
+fig_sc.update_layout(legend_title_text = "Brand") # Updates the histogram legend to be titled 'Transmission Type'
+fig_sc.update_xaxes(title_text=" Price") # Updates the X Axis to be titled "Days Since Listed"
+fig_sc.update_yaxes(title_text=" Odomoter") # Updates the Y axis to be title "Amount"
+
+
+st.write(fig_sc)
+
